38459efa1bd2b53d298191078b79ad01ec186fa8
Sakif-Hridoy <sakifworkspace@gmail.com>
2023-12-25 19:31:03 +0600

    use custom hooks and use interceptor

---
+++ src/hooks/useNumber.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const useNumber = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+useNumber.propTypes = {
+    
+};
+
+export default useNumber;
